3115906aea04903157e40687cd7e3649a171487c
Alina Danilenko <alinaporhalova@gmail.com>
2026-03-13 17:35:40 +0300

    Старый дизайн

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,5 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println("Hello, world!");
     }
 }
